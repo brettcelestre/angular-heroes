bf7e0212126e53ef7095fd7505cdf514744a9129
Brett Celestre <brett.celestre@accenture.com>
2017-07-14 15:50:48 -0700

    Inital commit

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,16 +1,46 @@
-import { BrowserModule } from '@angular/platform-browser';
-import { NgModule } from '@angular/core';
+import { BrowserModule }  from '@angular/platform-browser';
+import { NgModule }       from '@angular/core';
+import { FormsModule }    from '@angular/forms';
+import { RouterModule }   from '@angular/router';
+import { HttpModule }     from '@angular/http';
 
-import { AppComponent } from './app.component';
+// Components
+import { AppComponent }         from './app.component';
+import { HeroesComponent }      from './heroes.component';
+import { HeroDetailComponent }  from './hero-detail.component';
+import { DashboardComponent }   from './dashboard.component';
+
+// Services
+import { HeroService } from './hero.service';
+
+// Routes
+import { AppRoutingModule } from './app-routing.module';
+
+// In-Memory Web Api
+import { InMemoryWebApiModule } from 'angular-in-memory-web-api';
+import { InMemoryDataService }  from './in-memory-data.service';
 
 @NgModule({
-  declarations: [
-    AppComponent
-  ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    FormsModule,
+    HttpModule,
+    InMemoryWebApiModule.forRoot(InMemoryDataService),
+    AppRoutingModule
   ],
-  providers: [],
-  bootstrap: [AppComponent]
+  declarations: [
+    AppComponent,
+    HeroDetailComponent,
+    HeroesComponent,
+    DashboardComponent
+  ],
+  providers: [
+    HeroService
+  ],
+  bootstrap: [
+    AppComponent
+  ]
 })
+
+
 export class AppModule { }
